a98174372a212da95c2bc9bf5c1e876f9616b196
UrbanBjorkman <urban.bjorkman@gmail.com>
2014-09-24 14:39:54 +0200

    Inital add

---
+++ WebTimer/WebTimer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace WebTimer
+{
+	public class BackgroundHelper
+	{
+		private class JobHost : IRegisteredObject
+		{
+			public JobHost()
+			{
+				HostingEnvironment.RegisterObject(this);
+			}
+
+			public void Stop(bool immediate)
+			{
+				HostingEnvironment.UnregisterObject(this);
+			}
+
+			public void DoWork(Action work)
+			{
+				work();
+			}
+		}
+
+		public static void RunBackgroundJob(Action func, int intDelayMs = 0)
+		{
+			var thread = new Thread(() =>
+			{
+				try
+				{
+					var host = new JobHost();
+					if (intDelayMs > 0)
+					{
+						host.DoWork(() =>
+						{
+							//Initial delay
+							Thread.Sleep(intDelayMs);
+							func();
+						});
+					}
+					else
+					{
+						host.DoWork(func);
+					}
+				}
+				catch (Exception ex)
+				{
+					//TODO: Errorhandling
+					throw ex;
+				}
+
+			});
+			thread.Start();
+		}
+
+	}
+}
